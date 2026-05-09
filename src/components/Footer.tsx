d3e8e55a0df09767e7b20d712726c78172397418
Sovathara Prom <tharax3000@gmail.com>
2026-05-09 19:29:02 +0700

    adjustment on contact info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
-import { GitFork, Share2, Link2, Rss, Facebook } from "lucide-react";
+import { GitFork, Share2, Link2, Rss } from "lucide-react";
+
+function FacebookIcon({ size = 14 }: { size?: number }) {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="currentColor">
+      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+    </svg>
+  );
+}
 
 const socials = [
-  { icon: Facebook, label: "Facebook", href: "https://www.facebook.com/profile.php?id=61572607197768" },
+  { icon: FacebookIcon, label: "Facebook", href: "https://www.facebook.com/profile.php?id=61572607197768" },
   { icon: GitFork, label: "GitHub", href: "#" },
   { icon: Share2, label: "Twitter / X", href: "#" },
   { icon: Link2, label: "LinkedIn", href: "#" },
